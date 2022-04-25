2006edb682e3aecd3b937566803280253054b863
Kerembe <keremburak00@gmail.com>
2022-04-25 22:35:48 +0300

    <fix>[Files arranged and new file Operatör tested]

---
+++ Operatörler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operatörler Çalışıyor.");
+        }
+    }
+}

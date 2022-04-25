ffe530db5e36c9765a833870075a17d9e767a78c
Kerembe <keremburak00@gmail.com>
2022-04-25 18:52:36 +0300

    Değişkenler project created.

---
+++ Değişkenler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Değişkenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Informs user by writing directions on the terminal
+            Console.WriteLine("You gotta give me something, write anything: ");
+
+            //Creates a variable to store user input
+            string inputString;
+
+            //Stores user input into a variable
+            inputString = Console.ReadLine();
+
+            //Customary greetings of every new programmers first code
+            Console.WriteLine("Hello World!");
+
+            //Prints out user input on the terminal
+            Console.WriteLine(inputString);
+        }
+    }
+}

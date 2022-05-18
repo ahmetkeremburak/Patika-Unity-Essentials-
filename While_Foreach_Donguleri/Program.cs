0d8f41a26cc956bd6474298d490c546fe8ffcf69
Kerembe <keremburak00@gmail.com>
2022-05-18 16:39:34 +0300

    While ve Foreach döngüleri için oluşturulan alıştırma örnekleri

---
+++ While_Foreach_Donguleri/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace While_Foreach_Donguleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1'den girilen sayıya kadar olan sayıları toplayıp ortalamasını alan while döngüsü
+            Console.Write("Bir sayı alalım: ");
+            int input = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac <= input)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+
+            Console.WriteLine("Ortalama: " + toplam/input);
+
+            //'a'dan 'z'ye kadar tüm harfleri konsola yazdır.
+
+            char character = 'a';
+
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character ++;
+            }
+
+            //Foreach
+            // Evvela bir liste tanımlayalım, bu liste içinde işlem yapacağız.
+
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}

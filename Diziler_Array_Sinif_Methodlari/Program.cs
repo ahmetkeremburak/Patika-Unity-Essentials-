4a5be3016f2f1e0a3c5e5c9743c0a9f31446ed58
Kerembe <keremburak00@gmail.com>
2022-05-30 14:27:12 +0300

    Array Sınıf Methodları konusu oluşturuldu

---
+++ Diziler_Array_Sinif_Methodlari/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Diziler_Array_Sinif_Methodlari
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int[] sayi_dizisi = {23,12,4,86,72,3,11,17};
+            Console.WriteLine("*******Sırasız Dizi*******");
+
+            foreach (var sayi in sayi_dizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }   
+    }
+}
